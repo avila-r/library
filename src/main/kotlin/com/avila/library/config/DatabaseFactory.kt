9d6cccd7b67ce469c16848d9e57c7cfc2d34675b
avila-r <avila.dev@outlook.com>
2024-06-11 16:25:24 -0300

    feat: config datasource

---
+++ src/main/kotlin/com/avila/library/config/DatabaseFactory.kt
@@ -0,0 +1,51 @@
+package com.avila.library.config
+
+import com.typesafe.config.ConfigFactory
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+import io.ktor.server.config.*
+
+import org.ktorm.database.Database
+
+/**
+ * Database connection singleton object that's able to set up database connection.
+ */
+object DatabaseFactory {
+
+    private val config = getConfigurator()
+
+    private val db: Database
+
+    init {
+        db = Database.connect(getDataSource())
+    }
+
+    /**
+     * Retrieves database connection properties from the configuration.
+     * @return Properties object containing the database connection details.
+     */
+    private fun getDataSource(): HikariDataSource = HikariDataSource (
+
+        HikariConfig().apply {
+
+            jdbcUrl = config.property("db.url").getString()
+            driverClassName = config.property("db.driver").getString()
+            username = config.property("db.user").getString()
+            password = config.property("db.password").getString()
+            maximumPoolSize = 10
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_READ_COMMITTED"
+
+        }
+
+    )
+
+    /**
+     * Gets the database instance.
+     * @return Database instance connected to the configured data source.
+     */
+    fun getDatabase(): Database = db
+
+}
