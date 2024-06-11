54da0b49d6e9999315a5147cb60918e1bc66da0b
avila-r <avila.dev@outlook.com>
2024-06-11 16:24:29 -0300

    feat: config book routes

--- src/main/kotlin/com/avila/library/routes/BookRoutes.kt
+++ src/main/kotlin/com/avila/library/routes/BookRoutes.kt
@@ -1,29 +1,70 @@
-package com.avila.api.routes
+/**
+ * Routes related to book management in the application.
+ */
+package com.avila.library.routes
 
+import com.avila.library.schemas.HttpResultResponse
+import com.avila.library.services.BookServiceImpl
+import com.avila.library.utils.getCustomJsonObjectMapper
+
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.listBooksRoute() {
-    get {
-        // Todo: list books implementation
+/**
+ * Service instance for book-related operations.
+ */
+private val service = BookServiceImpl()
 
-        call.respondText { "List books" }
+/**
+ * Custom JSON object mapper configured for book-related responses.
+ */
+private val mapper = getCustomJsonObjectMapper()
+
+/**
+ * Routes
+ */
+fun Route.allBooksRoutes() {
+    listBooksRoute()
+    getBooksRoutes()
+    // postBooksRoute()
+    // updateBooksRoute()
+}
+
+/** Route to list all books */
+private fun Route.listBooksRoute() {
+    get {
+        call.respond(HttpStatusCode.OK, mapper.writeValueAsString(
+            HttpResultResponse(service.getAll()))
+        )
     }
 }
 
-fun Route.getBooksRoutes() {
+/** Route to get a specific book by ID */
+private fun Route.getBooksRoutes() {
     get("/id/{id}") {
 
+        val id: Long? = call.parameters["id"]?.toLongOrNull()
+
+        id?.let {
+            call.respond(HttpStatusCode.Found, mapper.writeValueAsString(
+                service.getById(id))
+            )
+        } ?: call.respond(HttpStatusCode.BadRequest)
+
     }
 
-    // ...
-}
-
-fun Route.postBooksRoute() {
+    // Additional routes can be configured here
 
 }
 
-fun Route.updateBooksRoute() {
+/** Route to add a new book */
+private fun Route.postBooksRoute() {
+    TODO("Not yet implemented")
+}
 
+/** Route to update an existing book */
+private fun Route.updateBooksRoute() {
+    TODO("Not yet implemented")
 }
