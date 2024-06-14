6fb151dd1e6ff13a5a3766ba031bd3e8ffba25c7
avila-r <avila.dev@outlook.com>
2024-06-13 23:09:53 -0300

    feat: implement save operation

--- src/main/kotlin/com/avila/library/services/BookService.kt
+++ src/main/kotlin/com/avila/library/services/BookService.kt
@@ -5,6 +5,7 @@ package com.avila.library.services
 
 import com.avila.library.schemas.Book
 import com.avila.library.config.DatabaseFactory.getDatabase
+import com.avila.library.schemas.BookRequest
 import com.avila.library.schemas.Books
 import com.avila.library.schemas.books
 
@@ -15,13 +16,16 @@ import org.ktorm.dsl.eq
 import org.ktorm.entity.*
 import org.slf4j.LoggerFactory
 
+import java.time.LocalDateTime
+
 /**
  * Interface defining operations for book management.
  */
 interface BookServiceContract {
     suspend fun getAll(): List<Book>
     suspend fun getById(id: Long): Book?
-    suspend fun save(book: Book)
+    suspend fun save(book: Book): Boolean
+    suspend fun save(request: BookRequest): Boolean
     suspend fun delete(id: Long)
 }
 
@@ -61,15 +65,46 @@ class BookService : BookServiceContract {
      * Saves a new book to the database.
      * @param book The book object to save.
      */
-    override suspend fun save(book: Book): Unit = withContext(Dispatchers.IO) {
-        this.runCatching {
+    override suspend fun save(book: Book): Boolean = withContext(Dispatchers.IO) {
+        return@withContext this.runCatching {
             database.books.add(book)
-        }.onSuccess {
-            logger.info("Saved book: $book")
-        }.onFailure {
-            logger.error("Error while trying to save book: $book", it)
-        }
+        }.fold (
+            onSuccess = {
+                logger.info("Saved book: $book")
+                true
+            },
+            onFailure = {
+                e -> logger.error("Error while trying to save book $book: ", e)
+                false
+            }
+        )
     }
+
+    /**
+     * Saves a new book to the database, but receiving and parsing a request.
+     * @param request The book object to save.
+     */
+    override suspend fun save(request: BookRequest): Boolean = save (
+        Book {
+            this.title = request.title
+            this.author = request.author
+            this.isbn = request.isbn
+            this.publicationDate = request.publicationDate
+            this.publisher = request.publisher
+            this.pages = request.pages
+            this.genre = request.genre
+            this.summary = request.summary
+            this.language = request.language
+            this.format = request.format
+            this.coverImageUrl = request.coverImageUrl
+            this.rating = 0.0f
+            this.price = request.price
+            this.availability = request.availability
+            this.tags = request.tags
+            this.addedDate = LocalDateTime.now()
+            this.location = request.location
+        }
+    )
 
     /**
      * Deletes a book from the database by its ID.
