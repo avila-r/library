ad7237f1703245fb4ebe57af35cf943b0630bbcf
avila-r <avila.dev@outlook.com>
2024-06-13 23:08:26 -0300

    docs: config swagger ui

--- src/main/kotlin/com/avila/library/handler/RoutingConfig.kt
+++ src/main/kotlin/com/avila/library/handler/RoutingConfig.kt
@@ -2,7 +2,9 @@ package com.avila.library.handler
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configRouting() {
 
@@ -16,5 +18,12 @@ fun Application.configRouting() {
      * Enable API router
      */
     handler()
+
+    /*
+     * Enable API docs
+     */
+    routing {
+        swaggerUI(path = "docs", swaggerFile = "openapi/documentation.yaml")
+    }
 
 }
